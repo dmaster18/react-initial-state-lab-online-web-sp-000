f5a3f3cd09fd2dd6afd699d639870414640813c7
David Strauch <dmaster18@gmail.com>
2021-01-24 00:21:57 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,20 @@
 // your Bomb code here!
+
+class Bomb extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      secondsLeft: this.props.initialCount;
+    }
+  }
+  
+  render() {
+    let response;
+    if (this.state.secondsLeft === 0) {
+      response = 'Boom!'
+    } else {
+     response = `${this.state.secondsLeft} seconds left before I go boom!`
+    }
+    return (response)
+  }
+}
